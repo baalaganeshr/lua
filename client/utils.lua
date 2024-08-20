f9cb8cfb5a9a0bd5e3a400a4fbd347256768a4b6
JaxDanger <jax@jaxdanger.com>
2024-08-19 20:01:29 -0400

    update

--- client/utils.lua
+++ client/utils.lua
@@ -4,27 +4,32 @@
 ---@param action string The action you wish to target
 ---@param data any The data you wish to send along with this action
 function SendReactMessage(action, data)
-  SendNUIMessage({
-    action = action,
-    data = data
-  })
+  SendNUIMessage(
+    {
+      action = action,
+      data = data
+    }
+  )
 end
 
-local currentResourceName = GetCurrentResourceName()
+function toggleNuiFrame(shouldShow)
+  SetNuiFocus(shouldShow, shouldShow)
+  SendReactMessage("setVisible", shouldShow)
+end
 
-local debugIsEnabled = GetConvarInt(('%s-debugMode'):format(currentResourceName), 0) == 1
-
---- A simple debug print function that is dependent on a convar
---- will output a nice prettfied message if debugMode is on
-function debugPrint(...)
-  if not debugIsEnabled then return end
-  local args <const> = { ... }
-
-  local appendStr = ''
-  for _, v in ipairs(args) do
-    appendStr = appendStr .. ' ' .. tostring(v)
+RegisterCommand(
+  "show-nui",
+  function()
+    toggleNuiFrame(true)
+    debugPrint("Show NUI frame")
   end
-  local msgTemplate = '^3[%s]^0%s'
-  local finalMsg = msgTemplate:format(currentResourceName, appendStr)
-  print(finalMsg)
-end
+)
+
+RegisterNUICallback(
+  "hideFrame",
+  function(_, cb)
+    toggleNuiFrame(false)
+    debugPrint("Hide NUI frame")
+    cb({})
+  end
+)
--- server/server_vrp.lua
+++
@@ -1,9 +0,0 @@
-local Tunnel = module("vrp", "lib/Tunnel")
-local Proxy = module("vrp", "lib/Proxy")
-
-local vRP = Proxy.getInterface("vRP")
-
-
-async(function()
-  vRP.loadScript("vrp_template", "server")		-- change vrp_tutorial to file name of mod
-end)
