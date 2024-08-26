a596b220285000fd780850f04ca4d689c36e061d
JaxDanger <jax@jaxdanger.com>
2024-08-25 21:15:10 -0400

    Update server.lua

--- server/server.lua
+++
@@ -1,9 +0,0 @@
-local vRPxReactTemplate = class("vRPxReactTemplate", vRP.Extension)
-vRPxReactTemplate.event = {}
-vRPxReactTemplate.tunnel = {}
-
-function vRPxReactTemplate:__construct()
-  vRP.Extension.__construct(self)
-end
-
-vRP:registerExtension(vRPxReactTemplate)
