344a6ba639dcd8e630836f76a6137f4987dbddce
JaxDanger <jax@jaxdanger.com>
2024-08-25 21:24:57 -0400

    updated config use

--- cfg/cfg.lua
+++ cfg/cfg.lua
@@ -1,3 +1,8 @@
-local cfg = {}
+-- config.lua
+cfg = {}
 
-return cfg
+-- Example cfguration variables
+cfg.ServerName = "My FiveM Server"
+cfg.MaxPlayers = 32
+cfg.StartingMoney = 1000
+cfg.EnablePvP = true
--- client/client.lua
+++ client/client.lua
@@ -1,2 +1,9 @@
 -- toggleNuiFrame(shouldShow)
 -- SendReactMessage(action, data)
+-- client/client.lua
+print("Server Name: " .. cfg.ServerName)
+print("Max Players: " .. cfg.MaxPlayers)
+
+-- server/server.lua
+print("Starting Money: " .. cfg.StartingMoney)
+print("PvP Enabled: " .. tostring(cfg.EnablePvP))
